863be8da4c36ae2c358b4ee8ec726ace1f059aae
Danny <csm012001@codercademy.edu.au>
2020-07-13 15:30:04 +0000

    Added the mutliplayer feature

--- main.py
+++ main.py
@@ -3,27 +3,22 @@ import os
 
 define = {'1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':10, 'Q':10, 'K':10, 'A':11}
 
-def cardgen():
-    import random
-    
-    number = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
-    face = ['J', 'Q', 'K', 'A']
-    
-    deck = (number+face)*4
-    random.shuffle(deck)
-    return deck
-
 def start():
     import copy
 
     print("\n\n__________________________________")
-    print("\nGame has begun...")
+    print("\nGame has begun...")   
 
-    start_hand(player)
+    for i in players:
+        start_hand(i)
     start_hand(dealer)
-    check(player)
+    for i in players:
+        check(i)
     check(dealer)
-    show_hand(player)
+    for i in players:
+        show_hand(i)
+        print(f"Total Bank: {i.bank}")
+        print(f"Betted Amount: {i.bet}")
 
     hand = copy.deepcopy(dealer.cards)
     hand[1] = '_'
@@ -31,32 +26,32 @@ def start():
     print(f"\nDealer's hand: {hand}")
     print("Total: ?")
 
-    print(f"\nBank Total: {player.bank}")
-    print(f"\nBetted Amount: {bet_amount}")
-
-def show_hand(user):
-    hand = str(user.cards).strip("[]").replace("'","")
-    print(f"\n{user.name}'s hand: {hand}")
-    print(f"Total: {total_sum(user)}")
-
-def choice(user):
-    while True:
+def choice():
+    for i in players:
         print("\n\n__________________________________")
-        decision = str(input("\nHIT or STAY\n"))
-        if decision.lower() == 'hit':
-            add_card(user)
-            show_hand(user)
-            print(f"\nBank Total: {player.bank}")
-            print(f"\nBetted Amount: {bet_amount}")
+        print(f"{i.name.upper()}'S TURN")
+        show_hand(i)
 
-        elif decision.lower() == 'stay':
-            break
-        else:
-            print("Please input either 'HIT' or 'STAY'")
-        
-        if check(user) == True:
-            print(f"{user.name.upper()} HAS BUSTED")
-            break
+        while True:
+
+            decision = str(input("\nHIT or STAY\n"))
+
+            if decision.lower() == 'hit':
+                add_card(i)
+                check(i)
+                show_hand(i)
+                print(f"\nBank Total: {i.bank}")
+                print(f"Betted Amount: {i.bet}")
+
+            elif decision.lower() == 'stay':
+                break
+
+            else:
+                print("Please input either 'HIT' or 'STAY'")
+            
+            if check(i) == True:
+                print(f"{i.name.upper()} HAS BUSTED")
+                break
 
 def ai():
     while True:
@@ -67,30 +62,32 @@ def ai():
         if check(dealer) == True:
             show_hand(dealer)
             print("DEALER HAS BUSTED")
-            break
+            break                       
 
 def winner():
-    if check(dealer) == True and check(player) == True:
-        return "BUST"
-    else:
-        if total_sum(dealer) == total_sum(player):
-            return "TIE"
+    for i in players:
+        if check(dealer) == True and check()
+    # if check(dealer) == True and check(player) == True:
+    #     return "BUST"
+    # else:
+    #     if total_sum(dealer) == total_sum(player):
+    #         return "TIE"
 
-        elif total_sum(dealer) > 21:
-            player.add_bank(bet_amount)
-            return player
+    #     elif total_sum(dealer) > 21:
+    #         player.add_bank(bet_amount)
+    #         return player
             
-        elif total_sum(player) > 21:
-            player.lose_bank(bet_amount)
-            return dealer
+    #     elif total_sum(player) > 21:
+    #         player.lose_bank(bet_amount)
+    #         return dealer
 
-        elif total_sum(dealer) > total_sum(player):
-            player.lose_bank(bet_amount)
-            return dealer
+    #     elif total_sum(dealer) > total_sum(player):
+    #         player.lose_bank(bet_amount)
+    #         return dealer
         
-        elif total_sum(player) > total_sum(dealer):
-            player.add_bank(bet_amount)
-            return player
+    #     elif total_sum(player) > total_sum(dealer):
+    #         player.add_bank(bet_amount)
+    #         return player
 
 def finish(user):
     print("\n\n__________________________________")
@@ -106,24 +103,29 @@ def finish(user):
         print(f"\n{user.name.upper()} HAS WON!!")
         show_hand(user)
 
-def bet(user):
-    print("\n\n__________________________________")
-    print(f"\nBank Total: {player.bank}")
+def bet():
     while True:
         try:
-            amount = int(input("\nEnter your bet...\n"))
-
-            if amount > user.bank:
-                print("This is over your bank budget!")   
-                print("please choose an appropriate amount corresponding to you bank amount")
-            elif amount < 0:
-                print("please choose an appropriate amount corresponding to you bank amount")
-            elif amount == 0:
-                print("Amount must be above zero")
-            else:
-                return amount            
+            for i in players:
+                while True:
+                    print("\n__________________________________")
+                    print(f"\nBank Total: {i.bank}")
+                    amount = int(input(f"\nEnter your bet {i.name}\n"))
+                    if amount > i.bank:
+                        print("This is over your bank budget!")   
+                        print("please choose an appropriate amount corresponding to you bank amount")
+                    elif amount < 0:
+                        print("please choose an appropriate amount corresponding to you bank amount")
+                    elif amount == 0:
+                        print("Amount must be above zero")
+                    else:
+                        i.bet = amount
+                        break
+            break
         except:
-            print("Please enter an interger number (E.G 0 1 2 3 50 ...)")
+            print("\n__________________________________")
+            print("\nPlease enter an interger number (E.G: 0 1 2 3 50 ...)")
+            continue
 
 def check_amount():
     if player.bank <= 0:
@@ -143,6 +145,35 @@ def check_amount():
             else:
                 print("Please choose either 'Yes' or 'No'")
 
+def nth_player():
+    while True:
+        try:
+            nth = int(input("\nHow many players?\n(Only up to 4 players)\n"))
+            if nth > 4 or nth < 1:
+                print("\nPlease enter an interger number between 1 - 4")
+            else: 
+                break
+        except:
+            print("\nPlease enter an interger number (E.G: 1 2 3 4)")
+    return nth
+
+def names(nth):
+    counter = 0
+    while counter < nth:
+        name = input("\nPlease enter your name...\n")
+        players.append(name)
+        counter += 1
+         
+def cardgen():
+    import random
+    
+    number = ['2', '3', '4', '5', '6', '7', '8', '9', '10']
+    face = ['J', 'Q', 'K', 'A']
+    
+    deck = (number+face)*4
+    random.shuffle(deck)
+    return deck
+
 def check(user):
     if 'A' in user.cards and total_sum(user) > 21:
         user.define_hand['A'] = 1
@@ -157,10 +188,14 @@ def total_sum(user):
         total += user.define_hand[i]
     return total
 
+def show_hand(user):
+    hand = str(user.cards).strip("[]").replace("'","")
+    print(f"\n{user.name}'s hand: {hand}")
+    print(f"Total: {total_sum(user)}")
+
 def start_hand(user):
     for i in range(2):
-        # user.cards.append(deck[i])
-        user.cards.append('A')
+        user.cards.append(deck[i])
     del deck[:2]
         
 def add_card(user):
@@ -182,10 +217,11 @@ def operating_system():
 class Player():
     import copy
     define_hand = copy.deepcopy(define)
-    def __init__(self, name, bank, cards=[]):
+    def __init__(self, name, bank, bet, cards=[]):
         self.cards = cards
         self.name = name
         self.bank = bank
+        self.bet = bet
 
     def lose_bank(self, bet):
         self.bank -= bet
@@ -218,17 +254,18 @@ def replay():
 
 print("Welcome to Blackjack")
 print("\nThe goal of the game is to get as close to 21 as possible and have the total higher then the dealer!\n")
-name = input("\nPlease enter your name...\n")
 
+players = []
 deck = cardgen()
-player = Player(name, 1000, [])
+names(nth_player())
+for i in range(len(players)):
+    players[i] = Player(players[i], 1000, 0, [])
 dealer = Dealer("Dealer", [])
 
 while True:
-    
-    bet_amount = bet(player)
+    bet()
     start()
-    choice(player)
+    choice()
     ai()
     finish(winner())
     again = check_amount()
